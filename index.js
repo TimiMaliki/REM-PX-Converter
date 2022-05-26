43bbf5eaac25d000b921b27461223bef0f2f9b68
TimiMaliki <malikitimi@gmail.com>
2022-05-26 12:01:40 -0700

    first commit

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ let myValue = squareRoot(0);
 
  
 console.log(myValue);
+
+function myVolume(value){
+      let result = value* length * breath * height;
+       return result;
+}
+
+
+let length = 2;
+let breath = 2;
+let height = 2;
+
+let product = myVolume(3);
+
+console.log(product);
