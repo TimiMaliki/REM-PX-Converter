ee7b0bb629f7f27f70dda95f9e311e55e4cb0fe1
TimiMaliki <malikitimi@gmail.com>
2022-05-28 15:34:36 -0700

    first commit

--- index-sr.js
+++ index-sr.js
@@ -1,3 +1,5 @@
+// exercise one
+
 function squareRoot(number){
       let result = number + Math.sqrt(9);
       
@@ -8,6 +10,8 @@ let myValue = squareRoot(0);
 
  
 console.log(myValue);
+
+// exercise two
 
 function myVolume(value){
       let result = value* length * breath * height;
@@ -23,9 +27,13 @@ let product = myVolume(1);
 
 console.log(product);
 
+// exercise three
+
 function myMultiply(digits){
       let result = digits *a * b * c * d * e;
        return result;
+
+       
 }
 
 let a = 2;
@@ -36,3 +44,19 @@ let e = 5;
 let finalResults = myMultiply(3);
 
 console.log(finalResults);
+
+//exercise SimpleInterest
+
+function simpleInterest(value){
+   let result = p* one + rate * time;
+  return result;
+};
+
+let p = 10000;
+let one = 1;
+let rate = 0.03875;
+let time = 5;
+
+let simple = simpleInterest();
+console.log(simple);
+
