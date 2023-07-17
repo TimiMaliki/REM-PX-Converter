9a3743e207913c05a69b3726b6e813586330b42a
TimiMaliki <malikitimi@gmail.com>
2023-07-17 16:26:27 +0100

    useState

--- UseState/src/App.jsx
+++ UseState/src/App.jsx
@@ -1,40 +1,61 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
 
-  const [setState , setUpdate] = useState({
-    count: 4 , theme: "Red"
-  })
+const App = () => {
+  const [myFavColor, updateTheColor] = useState({ color: "red" , change: ' white'})
 
-  const count = setState.count
-  const  theme = setState.theme
+  const color = myFavColor.color
 
-function decrement (){
-  //  setCount(previousCount  => previousCount - 1)
+  const  change = myFavColor.change
 
-  setUpdate(prevState => {
-    return {...prevState ,count: prevState.count - 1}
-  })
-
-
-
+function changingColor(){
+  updateTheColor(previousState => {return {...previousState , color:"blue" , change:"green"}
+})
 }
 
- function increment (){
-  // setCount(previousCount  => previousCount + 1)
-  setUpdate(prevState => {
-    return {...prevState ,count: prevState.count + 1}
-  })
- }
-
   return (
-    <>
-     <button onClick={decrement}>-</button>
-     {count}{theme}
-     <button onClick={increment}>+</button>
-    </>
+    <div>
+      <h2>My fav color <br /><br />
+        {color}{change}  <br /> <br />
+        but my pure color is
+        <button onClick={changingColor}></button>
+      </h2>
+    </div>
   )
 }
 
 export default App
+
+//   //example One
+//   const [setState , setUpdate] = useState({
+//     count: 4 , theme: "Red"
+//   })
+
+//   const count = setState.count
+//   const  theme = setState.theme
+
+// function decrement (){
+//   //  setCount(previousCount  => previousCount - 1)
+
+  // setUpdate(prevState => {
+  //   return {...prevState ,count: prevState.count - 1}
+  // })
+
+
+
+// }
+
+//  function increment (){
+//   // setCount(previousCount  => previousCount + 1)
+//   setUpdate(prevState => {
+//     return {...prevState ,count: prevState.count + 1}
+//   })
+//  }
+
+// export default App
+//    <button onClick={decrement}>-</button>
+//      {count}{theme}
+//      <button onClick={increment}>+</button> 
+
+
