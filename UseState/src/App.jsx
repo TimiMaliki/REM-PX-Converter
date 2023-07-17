675b32054dab17cd0ff56cd669223b2508031f06
TimiMaliki <malikitimi@gmail.com>
2023-07-17 17:11:08 +0100

    useState

--- UseState/src/App.jsx
+++ UseState/src/App.jsx
@@ -2,25 +2,51 @@ import React, { useState } from 'react'
 import './App.css'
 
 
+
+
 const App = () => {
-  const [myFavColor, updateTheColor] = useState({ color: "red" , change: ' white'})
 
-  const color = myFavColor.color
+  const [origin , updates] = useState({
+    name: 'Timi',
+    age: 24,
+    occupation: 'front-End', 
+    degree: 'Bsc',
+    relationshipStatus: 'Single',
+    netWorth: 0,
+  })
 
-  const  change = myFavColor.change
 
-function changingColor(){
-  updateTheColor(previousState => {return {...previousState , color:"blue" , change:"green"}
-})
-}
+    const name = origin.name
+    const   age = origin.age
+    const   occupation = origin.occupation
+    const   degree = origin.degree
+    const   relationshipStatus = origin.relationshipStatus
+    const   netWorth = origin.netWorth
+
+    function BioUpdate(){
+      updates(previous => {return{ ...previous , name:'TimiMaliki' , age : 29 ,
+    occupation: 'FullStack Developer' , relationshipStatus : "Married",  netWorth: 10956789450 , degree:'PHD'
+    }})
+    }
 
   return (
     <div>
-      <h2>My fav color <br /><br />
-        {color}{change}  <br /> <br />
-        but my pure color is
-        <button onClick={changingColor}></button>
-      </h2>
+      <h1> My BioData now</h1>
+            <ul>
+              <li>{name}</li>
+              <li>{age}</li>
+              <li>{occupation}</li>
+              <li>{degree}</li>
+              <li>{relationshipStatus}</li>
+              <li>{netWorth}</li>
+            </ul>
+
+            <h1> My BioData in 5years
+
+              <br />
+
+              <button onClick={BioUpdate}>Click Me</button>
+            </h1>
     </div>
   )
 }
@@ -52,10 +78,39 @@ export default App
 //     return {...prevState ,count: prevState.count + 1}
 //   })
 //  }
-
-// export default App
 //    <button onClick={decrement}>-</button>
 //      {count}{theme}
 //      <button onClick={increment}>+</button> 
+// export default App
 
 
+
+
+
+
+ //   // exampleTwo
+// const App = () => {
+ 
+  //   const [myFavColor, updateTheColor] = useState({ color: "red" , change: ' white'})
+  
+  //   const color = myFavColor.color
+  
+  //   const  change = myFavColor.change
+  
+  // function changingColor(){
+  //   updateTheColor(previousState => {return {...previousState , color:"blue" , change:"green"}
+  // })
+  // }
+  
+  //   return (
+  //     <div>
+  //       <h2>My fav color <br /><br />
+  //         {color}{change}  <br /> <br />
+  //         but my pure color is
+  //         <button onClick={changingColor}></button>
+  //       </h2>
+  //     </div>
+  //   )
+  // }
+  
+  // export default App
