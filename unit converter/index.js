b616d68fe73c84da6eacda4e198122f1fa11da26
TimiMaliki <malikitimi@gmail.com>
2023-09-18 02:54:35 +0100

    unit converter

---
+++ unit converter/index.js
@@ -0,0 +1,42 @@
+const remValue = document.querySelector(".rem");
+const pxValue = document.querySelector(".px");
+const remValueInput = document.querySelector(".remNum");
+const pxValueInput = document.querySelector(".pxNum");
+const button = document.querySelector(".btn");
+const resetBtn = document.querySelector(".reset-btn");
+
+
+
+
+button.addEventListener("click", (e) => {
+    remValueField()
+    pxValueField()
+})
+
+
+resetBtn.addEventListener("click", (e) => {
+    pxValue.innerHTML = "px"
+    remValue.innerHTML = "rem"
+    remValueInput.value = ''
+    pxValueInput.value  =''
+})
+
+function remValueField(){
+
+    pxValue.innerHTML = `${remValueInput.value * 16 } px` 
+ 
+}
+
+
+function pxValueField(){
+    remValue.innerHTML = `${pxValueInput.value / 16} rem`
+    
+}
+
+
+
+
+
+
+
+
